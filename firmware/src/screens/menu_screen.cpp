09db0fa0bfd0969edb9daa0edc42a74ab9289f59
contactsimonwilson <contactsimonwilson@gmail.com>
2026-06-17 21:41:05 +1200

    Add confirm dialog

--- firmware/src/screens/menu_screen.cpp
+++ firmware/src/screens/menu_screen.cpp
@@ -13,6 +13,13 @@ static const char *TAG = "PUBREMOTE-MENU_SCREEN";
 
 static bool confirm_reset = false;
 
+enum PendingAction {
+  PENDING_NONE,
+  PENDING_SHUTDOWN,
+  PENDING_RESET
+};
+static PendingAction pending_action = PENDING_NONE;
+
 extern "C" void handle_menu_shutdown_long_press() {
   confirm_reset = !confirm_reset;
   slint::invoke_from_event_loop([]() {
@@ -26,9 +33,35 @@ extern "C" void setup_menu_properties() {
   const auto &state = get_slint_window()->global<UiState>();
   
   confirm_reset = false;
+  pending_action = PENDING_NONE;
   state.set_shutdown_text("Shutdown");
   state.on_menu_shutdown_long_press([]() {
     handle_menu_shutdown_long_press();
+  });
+
+  state.on_confirm_dialog_accepted([]() {
+    slint::invoke_from_event_loop([]() {
+      get_slint_window()->global<UiState>().set_show_confirm_dialog(false);
+    });
+    if (pending_action == PENDING_RESET) {
+      reset_all_settings();
+      esp_restart();
+    } else if (pending_action == PENDING_SHUTDOWN) {
+      enter_sleep();
+    }
+    pending_action = PENDING_NONE;
+  });
+
+  state.on_confirm_dialog_rejected([]() {
+    slint::invoke_from_event_loop([]() {
+      const auto &state = get_slint_window()->global<UiState>();
+      state.set_show_confirm_dialog(false);
+      if (confirm_reset) {
+        confirm_reset = false;
+        state.set_shutdown_text("Shutdown");
+      }
+    });
+    pending_action = PENDING_NONE;
   });
   
   if (pairing_state == PAIRING_STATE_PAIRED) {
@@ -107,10 +140,22 @@ extern "C" void handle_open_about() {
 
 extern "C" void handle_menu_shutdown() {
   ESP_LOGI(TAG, "Shutdown button pressed");
-  if (confirm_reset) {
-    reset_all_settings();
-    esp_restart();
-  } else {
-    enter_sleep();
-  }
+  slint::invoke_from_event_loop([]() {
+    const auto &state = get_slint_window()->global<UiState>();
+    if (confirm_reset) {
+      pending_action = PENDING_RESET;
+      state.set_confirm_dialog_title("Factory Reset");
+      state.set_confirm_dialog_message("This will wipe all settings. Proceed?");
+      state.set_confirm_dialog_confirm_text("Reset");
+      state.set_confirm_dialog_variant("danger");
+    } else {
+      pending_action = PENDING_SHUTDOWN;
+      state.set_confirm_dialog_title("Power Off");
+      state.set_confirm_dialog_message("Are you sure you want to turn off the remote?");
+      state.set_confirm_dialog_confirm_text("Shutdown");
+      state.set_confirm_dialog_variant("standard");
+    }
+    state.set_confirm_dialog_cancel_text("Cancel");
+    state.set_show_confirm_dialog(true);
+  });
 }
